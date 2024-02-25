14284978c3408db7b1405434e6288d41d8d317c7
nathanredblur <jon.nathan.rich@gmail.com>
2024-02-25 17:58:48 -0500

    counter with class

--- app/count.js
+++ app/count.js
@@ -1,7 +1,29 @@
 exports = typeof window === 'undefined' ? global : window;
 
-exports.countAnswers = {
-  count: function (start, end) {
+class Counter {
+  constructor(startNumber, endNumber) {
+    this.starNumber = startNumber;
+    this.endNumber = endNumber;
+    this.count = startNumber;
+    this.start();
+  }
+  start() {
+    this.interval = setInterval(() => {
+      console.log(this.count);
+      this.count += 1;
+      if (this.count > this.endNumber) {
+        this.cancel();
+      }
+    }, 100);
+  }
+  cancel() {
+    this.interval && clearInterval(this.interval);
+  }
+}
 
+exports.countAnswers = {
+  count: (start, end) => {
+    const counter = new Counter(start, end);
+    return counter;
   }
 };
