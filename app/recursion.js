a0868dfd429a627551458dff381de3af6a05be5f
nathanredblur <jon.nathan.rich@gmail.com>
2024-02-26 07:57:00 -0500

    recursion WIP

--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,38 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
+    const files = [];
+    const shouldAdd = !dirName || dirName === data.dir;
+
+    data.files.forEach((element) => {
+      if (typeof element === 'string') {
+        if (shouldAdd) {
+          files.push(element);
+        }
+      } else {
+        let folderFiles = [];
+        if (shouldAdd) {
+          folderFiles = this.listFiles(element);
+        } else {
+          folderFiles = this.listFiles(element, dirName);
+        }
+
+        files.push(...folderFiles);
+      }
+    });
+    return files;
+  },
+
+  permute: function (arr) {
 
   },
 
-  permute: function(arr) {
+  fibonacci: function (n) {
 
   },
 
-  fibonacci: function(n) {
-
-  },
-
-  validParentheses: function(n) {
+  validParentheses: function (n) {
 
   }
 };
