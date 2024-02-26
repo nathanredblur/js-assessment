605eef561301c8f466dcf862746b0f47a17efefc
nathanredblur <jon.nathan.rich@gmail.com>
2024-02-26 07:55:17 -0500

    tuning eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true,
-    "node": true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true
   },
-  "extends": "eslint:recommended",
-  "overrides": [
+  'extends': 'eslint:recommended',
+  'overrides': [
     {
-      "env": {
-        "node": true
+      'env': {
+        'node': true
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
+      'files': [
+        '.eslintrc.{js,cjs}'
       ],
-      "parserOptions": {
-        "sourceType": "script"
+      'parserOptions': {
+        'sourceType': 'script'
       }
     }
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
+  'parserOptions': {
+    'ecmaVersion': 'latest'
   },
-  "rules": {
-    "no-global-assign": ["error", { "exceptions": ["exports"] }],
-    "no-implicit-globals": "off",
-    "object-curly-spacing": ["error", "always"]
+  'rules': {
+    'no-global-assign': ['error', { 'exceptions': ['exports'] }],
+    'no-implicit-globals': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-reflect': ['off']
   }
-}
+};
