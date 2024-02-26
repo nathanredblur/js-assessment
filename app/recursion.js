ebce3426c18664e2091b2f99c9cdb53e74778bd7
nathanredblur <jon.nathan.rich@gmail.com>
2024-02-26 12:08:16 -0500

    recursion and string functions

--- app/recursion.js
+++ app/recursion.js
@@ -25,14 +25,60 @@ exports.recursionAnswers = {
   },
 
   permute: function (arr) {
+    // Heap's method for generating permutations
+    const length = arr.length;
+    const result = [arr.slice()];
+    const currentIndex = new Array(length).fill(0);
+    let index = 1;
+    let tempIndex;
 
+    const swap = (a, b) => {
+      [arr[a], arr[b]] = [arr[b], arr[a]];
+    };
+
+    while (index < length) {
+      if (currentIndex[index] < index) {
+        // if index is odd then tempIndex = currentIndex[index]
+        tempIndex = index % 2 && currentIndex[index];
+        swap(index, tempIndex);
+        ++currentIndex[index];
+        index = 1;
+        result.push(arr.slice());
+      } else {
+        currentIndex[index] = 0;
+        ++index;
+      }
+    }
+    return result;
   },
 
-  fibonacci: function (n) {
+  fibonacci: function (number) {
+    const memo = [0, 1];
 
+    const fib = (n) => {
+      if (typeof memo[n] === 'undefined') {
+        memo[n] = fib(n - 1) + fib(n - 2);
+      }
+      return memo[n];
+    };
+
+    return fib(number);
   },
 
   validParentheses: function (n) {
-
+    const result = [];
+    const generate = (left, right, str) => {
+      if (left === 0 && right === 0) {
+        result.push(str);
+      }
+      if (left > 0) {
+        generate(left - 1, right + 1, str + '(');
+      }
+      if (right > 0) {
+        generate(left, right - 1, str + ')');
+      }
+    };
+    generate(n, 0, '');
+    return result;
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,50 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
+  reduceString: function (str, amount) {
+    let currentLetter = '';
+    let letterCount = 0;
+    let result = '';
 
+    str.split('').forEach((letter) => {
+      if (currentLetter === letter) {
+        letterCount += 1;
+        if (letterCount <= amount) {
+          result += letter;
+        }
+      } else {
+        letterCount = 1;
+        currentLetter = letter;
+        result += letter;
+      }
+    });
+    return result;
   },
 
-  wordWrap: function(str, cols) {
+  wordWrap: function (str, cols) {
+    const words = str.split(' ');
+    let currentLine = '';
+    let result = '';
 
+    words.forEach((word, index) => {
+      if (currentLine.length + word.length <= cols) {
+        currentLine += word + ' ';
+      } else {
+        if (currentLine.length) {
+          result += currentLine.trim() + '\n';
+        }
+        currentLine = word + ' ';
+      }
+
+      if (index === words.length - 1) {
+        result += currentLine.trim();
+      }
+    });
+    return result;
   },
 
-  reverseString: function(str) {
-
+  reverseString: function (str) {
+    return str.split('').reverse().
+      join('');
   }
 };
